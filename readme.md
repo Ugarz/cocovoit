0971ee81a318ae780b7a0eedf1ddec9b4e9687bb
Ugo <ugo.arzur@morning.com>
2018-04-10 15:06:45 +0200

    up

--- readme.md
+++ readme.md
@@ -8,3 +8,6 @@ npm start          // Run the server
 npm test           // Run the tests
 npm run test:live  // Run the tests, live
 ```
+
+https://youtu.be/3EnE9FylZXg
+[![Cocovoit - Les Lopez](https://cdn.koreus.com/thumbshigh/201703/les-lopez-cocovoit.jpg)](https://youtu.be/3EnE9FylZXg)
--- test.js
+++ test.js
@@ -7,7 +7,6 @@ const { createPassenger, createCocovoit } = require("./core/covoit.js");
 describe("Testing passenger creation", function() {
   describe("Should create a Passenger", function() {
     it("Should return a passenger with data", function() {
-      
       const uPassenger = createPassenger("ugo", 5, true);
       const cPassenger = createPassenger("camille", 3, true);
       const jPassenger = createPassenger("julien", 2, true);
@@ -24,16 +23,17 @@ describe("Testing passenger creation", function() {
 
 describe("Testing Cocovoit", function() {
   it("Should create Users", function() {
-    
     const uPassenger = createPassenger("ugo", 5, true);
     const cPassenger = createPassenger("camille", 3, true);
     const jPassenger = createPassenger("julien", 2, true);
+
     cPassenger.should.have.property("name", "camille");
     jPassenger.should.have.property("name", "julien");
     uPassenger.should.have.property("name", "ugo");
     uPassenger.should.have.property("name").with.lengthOf(3);
     uPassenger.number_per_week.should.be.exactly(5).and.be.a.Number();
   });
+  
   it("Should create Cocovoit", function() {
     const options = {
         kms: 35.5,
