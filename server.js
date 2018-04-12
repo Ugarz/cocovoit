1d6fa043d4dfc08af13663e224383edfabe8e913
Ugo <ugo.arzur@morning.com>
2018-04-12 15:34:29 +0200

    update server

--- server.js
+++ server.js
@@ -3,13 +3,22 @@ const { createPassenger, createCocovoit } = require("./core/covoit.js");
 const options = {
   kms: 35.5,
   price_per_kms: 1.62,
-  passengers: ["ugo", "camille", "julien"],
+  passengers: [],
   comeBack: true,
-  conductor: "ugo"
+  conductor: "ugo",
+  car: {
+    model: "308",
+    brand: "Peugeot",
+    consumption: 7
+  }
 };
 
 const p = createPassenger('ugo', 5, true);
 const c = createCocovoit(options);
 
+c.addPassengers(p)
+c.getPassengers()
+
 console.log('User', p)
 console.log('Cocovoit', c)
+console.log(c.calc())
